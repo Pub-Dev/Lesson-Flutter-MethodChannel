2ee4c4c986c3c3fd67931d48f34e6a550577e78b
Rafael Nagai <rafakenji23@gmail.com>
2022-09-17 14:23:12 -0300

    some fix in ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,32 +7,34 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+      
+      let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+      let batteryChannel = FlutterMethodChannel(name: "my_app/method/battery", binaryMessenger: controller.binaryMessenger)
+      
+      batteryChannel.setMethodCallHandler({
+          (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+          guard call.method == "getNativeBatteryLevel" else {
+              result(FlutterMethodNotImplemented)
+              return
+          }
+          self.receiveBatteryLevel(result: result)
+      })
 
-    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
-    let batteryChannel = FlutterMethodChannel(name: "my_app/method/battery",
-                                              binaryMessenger: controller.binaryMessenger)
-    batteryChannel.setMethodCallHandler({
-      (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
-      guard call.method == "getNativeBatteryLevel" else {
-        result(FlutterMethodNotImplemented)
-        return
-      }
-      self?.receiveBatteryLevel(result: result)
-    })
+      GeneratedPluginRegistrant.register(with: self)
+      return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
 
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  private func receiveBatteryLevel(result: FlutterResult) {
+    let device = UIDevice.current
+    device.isBatteryMonitoringEnabled = true
+    if device.batteryState == UIDevice.BatteryState.unknown {
+      result(FlutterError(code: "UNAVAILABLE",
+                          message: "Battery level not available.",
+                          details: nil))
+    } else {
+      result(Int(device.batteryLevel * 100))
+    }
   }
 }
 
-private func receiveBatteryLevel(result: FlutterResult) {
-  let device = UIDevice.current
-  device.isBatteryMonitoringEnabled = true
-  if device.batteryState == UIDevice.BatteryState.unknown {
-    result(FlutterError(code: "UNAVAILABLE",
-                        message: "Battery level not available.",
-                        details: nil))
-  } else {
-    result(Int(device.batteryLevel * 100))
-  }
-}
+
